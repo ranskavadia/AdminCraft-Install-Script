c9d7551d99a9378757e555c28c1e6cc388da3da1
root <root@d-mintmachine.(none)>
2012-08-05 13:38:11 +0200

    added installadmincraft.sh

---
+++ installadmincraft.sh
@@ -0,0 +1,12 @@
+sudo apt-get install sqlite3 python-setuptools git
+git clone git://github.com/alfg/AdminCraft.git
+cd AdminCraft
+sudo python setup.py install
+vim config.ini
+sudo cp scripts/minecraft /etc/init.d/
+sudo vim /etc/init.d/minecraft
+sudo chmod a+x /etc/init.d/minecraft
+sudo update-rc.d minecraft defaults
+cd AdminCraft
+echo "All done! Type python run.py to start AdminCraft of nohup python run.py & to run in background"
+
